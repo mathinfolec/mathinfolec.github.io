611d6009a956b3241137d475a323c1b89cceaa7f
mathinfolec <40843404+mathinfolec@users.noreply.github.com>
2020-08-05 21:08:19 +0900

    Delete freeze.js

--- flow/js/freeze.js
+++
@@ -1,9 +0,0 @@
-for (let i in parts) {
-    for (let j in parts[i]) {
-        Object.freeze(parts[i][j]);
-    }
-    Object.freeze(parts[i]);
-}
-Object.freeze(parts);
-Object.freeze(options);
-console.log("freezed");
