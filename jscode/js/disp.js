0d03b6f2e2417bb7a44ed1813db687fcf59f43ae
mathinfolec <40843404+mathinfolec@users.noreply.github.com>
2020-08-10 07:18:40 +0900

    一部レイアウト、表記ゆれの修正

--- jscode/js/disp.js
+++ jscode/js/disp.js
@@ -1,11 +1,13 @@
 const showTextArea = function () {
-    document.getElementById("ta-code").style.display = "block";
+    document.getElementById("ta_code").style.display = "block";
     document.getElementById("button_conv").style.display = "inline-block";
-    document.getElementById("ta-open").style.display = "none";
+    document.getElementById("ta_open").style.display = "none";
+    document.getElementById("code_out").style.display = "none";
     reqConv();
 }
 const hideTextArea = function () {
-    document.getElementById("ta-code").style.display = "none";
+    document.getElementById("ta_code").style.display = "none";
     document.getElementById("button_conv").style.display = "none";
-    document.getElementById("ta-open").style.display = "inline-block";
+    document.getElementById("ta_open").style.display = "inline-block";
+    document.getElementById("code_out").style.display = "block";
 }
