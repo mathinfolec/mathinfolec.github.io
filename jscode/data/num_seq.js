2643fc5121356a418f5b687116ed1ef91b8fb71b
mathinfolec <40843404+mathinfolec@users.noreply.github.com>
2020-08-15 15:05:29 +0900

    問題の追加

---
+++ jscode/data/num_seq.js
@@ -0,0 +1,16 @@
+qdata["num_seq"] = {
+    title: "等差数列の出力",
+    section: "loop",
+    exp: "整数a,bが入力として与えられます。初項a,公差bの等差数列を第5項まで出力してください。",
+    defCode: "let a=read(), b=read();",
+    sample: [
+        {
+            in: "2 1",
+            out: "2\n3\n4\n5\n6"
+        },
+        {
+            in: "10 -3",
+            out: "10\n7\n4\n1\n-2"
+        }
+    ]
+};
