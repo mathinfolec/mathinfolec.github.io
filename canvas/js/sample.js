733175e4f5db2de30342494e9dc2eb39386ddfc0
mathinfolec <40843404+mathinfolec@users.noreply.github.com>
2020-09-01 15:06:23 +0900

    fancyboxのオプション追加

--- canvas/js/sample.js
+++ canvas/js/sample.js
@@ -5,12 +5,18 @@ const setupSamples = function () {
     let u = getUlSamplesId();
     for (let i in samples) {
         let l = document.createElement("li");
-        l.innerHTML = "<a data-fancybox='images' href='img/" + i + ".gif'>" + samples[i] + "</a>";
+        l.innerHTML = "<a data-touch='false' data-fancybox='images' href='img/" + i + ".gif'>" + samples[i] + "</a>";
         u.appendChild(l);
     }
     $.fancybox.defaults.keyboard = false;
     $.fancybox.defaults.arrows = false;
     $.fancybox.defaults.infobar = false;
-    $.fancybox.defaults.buttons = ["download", "close"];
+    $.fancybox.defaults.buttons = ["close"];
     $.fancybox.defaults.wheel = false;
+    $.fancybox.defaults.protect = true;
+    $.fancybox.defaults.touch.vertical = false;
+    $.fancybox.defaults.touch.momentum = false;
+    $.fancybox.defaults.clickContent = function (current, event) {
+        return current.type == false;
+    }
 }
