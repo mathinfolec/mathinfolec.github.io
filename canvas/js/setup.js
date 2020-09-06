f4e2d64de093b1fc10f0237f9bfe935d3a6d38e6
mathinfolec <40843404+mathinfolec@users.noreply.github.com>
2020-09-06 20:56:56 +0900

    何も実行しなかった際に終了確認メッセージが出ないよう修正

--- canvas/js/setup.js
+++ canvas/js/setup.js
@@ -1,12 +1,13 @@
 const setup = function () {
-    addLog("open");
     stage = new createjs.Stage("canvas");
     bg = new createjs.Shape();
     bg.graphics.beginFill("black").drawRect(0, 0, w, h);
     bg.x = 0;
     bg.y = 0;
     stage.addChild(bg);
+    prevURLStr = location.href;
     setupSlots();
     setupSamples();
     init();
+    addLog("open", prevURLStr);
 }
